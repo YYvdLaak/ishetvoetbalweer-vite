939bc4113e082627343ca9325b6164f35d21741f
Yannick van der Laak <yannick@levarne.nl>
2021-11-25 20:20:59 +0100

    Basic ishetvoetbalweer homepage

---
+++ src/components/VoetbalWeer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template lang="pug">
+.hello-world
+  h1 IS HET VOETBALWEER?
+  h1.msg JA
+</template>
+
+<style scoped>
+h1 {
+  font-size: 50pt;
+  color: white;
+}
+</style>
